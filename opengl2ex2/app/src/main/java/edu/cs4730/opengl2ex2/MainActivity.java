6789ab574ba2d1a2bf03b12ef72837190c771e67
Jim Ward <seker@uwyo.edu>
2017-11-06 08:36:03 -0700

    updated to API 27, studio 3 and add immersive mode

--- opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/MainActivity.java
+++ opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.cs4730.opengl2ex2;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 /*
@@ -15,15 +17,41 @@ import android.view.Window;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
         // Turn off the window's title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        
-		super.onCreate(savedInstanceState);
-		//the bulk of the code the make sure we are using opengl2 is in the extended glsurfaceview.
-		setContentView(new myGlSurfaceView(this));
-	}
 
+        super.onCreate(savedInstanceState);
+        //the bulk of the code the make sure we are using opengl2 is in the extended glsurfaceview.
+        setContentView(new myGlSurfaceView(this));
+    }
+
+    /*
+     * turns on immersive mode.  to get the navigation back, the user needs to swipe up
+     * from the bottom of the screen.
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  //this line is api 19+
+            } else {
+                getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+        }
+    }
 }
--- opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/myGlSurfaceView.java
+++ opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/myGlSurfaceView.java
@@ -5,34 +5,34 @@ import android.opengl.GLSurfaceView;
 
 public class myGlSurfaceView extends GLSurfaceView {
 
-	LessonOneRenderer myRender;
-	
-	public myGlSurfaceView(Context context) {
-		super(context);
+    LessonOneRenderer myRender;
+
+    public myGlSurfaceView(Context context) {
+        super(context);
         // Create an OpenGL ES 2.0 context.
         setEGLContextClientVersion(2);
 
-        super.setEGLConfigChooser(8 , 8, 8, 8, 16, 0);
+        super.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         // Set the Renderer for drawing on the GLSurfaceView
         myRender = new LessonOneRenderer();
         setRenderer(myRender);
- 
+
         // Render the view only when there is a change in the drawing data
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
-	}
-	
+    }
+
     @Override
     public void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
         myRender.onPause();
     }
- 
+
     @Override
     public void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
         myRender.onResume();
     }
- 
+
 }
