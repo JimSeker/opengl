71d78cc89523a30b50bc6d14e84a2bb73ddd9524
Jim Ward <seker@uwyo.edu>
2017-11-06 08:41:17 -0700

    updated to API 27, studio 3.0.0 and add immersive mode

--- opengl3ex2/app/src/main/java/edu/cs4730/opengl3ex2/MainActivity.java
+++ opengl3ex2/app/src/main/java/edu/cs4730/opengl3ex2/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 /*
  * simple example where the GLSurfaceView is an extended class and the bulk
@@ -43,4 +45,30 @@ public class MainActivity extends Activity {
         return ( info.reqGlEsVersion >= 0x30000 );
     }
 
+    /*
+ * turns on immersive mode.  to get the navigation back, the user needs to swipe up
+ * from the bottom of the screen.
+ */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  //this line is api 19+
+            } else {
+                getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+        }
+    }
 }
