63171ccb2bd8d89744563c97fba1083a043d7725
Jim Ward <seker@uwyo.edu>
2020-02-19 11:19:43 -0700

    updated to API 29, some general clean up as well.

--- opengl3ex1/app/src/main/java/edu/cs4730/opengl3ex1/MainActivity.java
+++ opengl3ex1/app/src/main/java/edu/cs4730/opengl3ex1/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.opengl.GLSurfaceView;
+import android.view.View;
 import android.view.Window;
 
 /*
@@ -24,58 +26,74 @@ import android.view.Window;
 
 public class MainActivity extends Activity {
 
-    /** Hold a reference to our GLSurfaceView */
+    /**
+     * Hold a reference to our GLSurfaceView
+     */
     private GLSurfaceView mGLSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Turn off the window's title bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
 
         mGLSurfaceView = new GLSurfaceView(this);
-        if ( detectOpenGLES30() )
-        {
+        if (detectOpenGLES30()) {
 // Tell the surface view we want to create an OpenGL ES 3.0-compatible
 // context, and set an OpenGL ES 3.0-compatible renderer.
-            mGLSurfaceView.setEGLContextClientVersion ( 3 );
+            mGLSurfaceView.setEGLContextClientVersion(3);
             // Set the renderer to our demo renderer, defined below.
             mGLSurfaceView.setRenderer(new LessonOneRenderer());
-        }
-        else
-        {
+        } else {
             // This is where you could create an OpenGL ES 2.0 and/or 1.x compatible
             // renderer if you wanted to support both ES 1 and ES 2.
             Log.e("HelloTriangle", "OpenGL ES 3.0 not supported on device. Exiting...");
             finish();
         }
-        setContentView ( mGLSurfaceView );
+        setContentView(mGLSurfaceView);
 
     }
 
 
-
-    private boolean detectOpenGLES30()
-    {
+    private boolean detectOpenGLES30() {
         ActivityManager am =
-                ( ActivityManager ) getSystemService ( Context.ACTIVITY_SERVICE );
+            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo info = am.getDeviceConfigurationInfo();
-        return ( info.reqGlEsVersion >= 0x30000 );
+        return (info.reqGlEsVersion >= 0x30000);
     }
 
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         // The activity must call the GL surface view's onResume() on activity onResume().
         super.onResume();
         mGLSurfaceView.onResume();
     }
+
     @Override
-    protected void onPause()
-    {
+    protected void onPause() {
         // The activity must call the GL surface view's onPause() on activity onPause().
         super.onPause();
         mGLSurfaceView.onPause();
+    }
+
+    /**
+     * turns on immersive mode.  to get the navigation back, the user needs to swipe up
+     * from the bottom of the screen.
+     */
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  //this line is api 19+
+
+        }
     }
 
 }
