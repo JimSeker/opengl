3f5fa382e887a3eb959831b027f27b9bb55c3d11
Jim Ward <JimSeker@users.noreply.github.com>
2024-11-18 09:30:31 -0700

    updated to api 35 and android 15 layout.

--- HelloOpenGLES20/app/src/main/java/com/example/android/opengl/MainActivity.java
+++ HelloOpenGLES20/app/src/main/java/com/example/android/opengl/MainActivity.java
@@ -15,17 +15,21 @@
  */
 package com.example.android.opengl;
 
-import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 /*
- * There is not much here, but at the bottom, it setups immersive Mode, so that only the app
- * shows, and the use needs to swipe up from the bottom to get the navigation buttons to appear.
+ * There is not much here,  it setups the activity and all the insets, then adds
+ * the opengl to the layout, so it will display.
  */
 
-public class OpenGLES20Activity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private GLSurfaceView mGLView;
 
@@ -33,10 +37,17 @@ public class OpenGLES20Activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
         // Create a GLSurfaceView instance and set it
         // as the ContentView for this Activity
+        LinearLayout myLayout = findViewById(R.id.main);
         mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
+        myLayout.addView(mGLView);
     }
 
     @Override
@@ -56,18 +67,5 @@ public class OpenGLES20Activity extends Activity {
         // If you de-allocated graphic objects for onPause()
         // this is a good place to re-allocate them.
         mGLView.onResume();
-    }
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        }
     }
 }
