73605bff3ebb8240b50fce4a7538599b06a41f56
Jim Ward <JimSeker@users.noreply.github.com>
2026-09-24 08:20:05 -0600

    updated deprecated immersive mode
    Had to add the appcompatactivity and all the necessary pieces for it to work as well.

--- opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/MainActivity.java
+++ opengl2ex2/app/src/main/java/edu/cs4730/opengl2ex2/MainActivity.java
@@ -6,16 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 /*
  * simple example where the GLSurfaceView is an extended class and the bulk
  * of the code is in there and the render of course.
  *
- * Note, there is no xml layout for this example.  It's all done in onCreate, myGlSurfaceView and the render.
+ * Note, there is no XML layout for this example.  It's all done in onCreate, myGlSurfaceView and the render.
  *
  * The render is from http://www.learnopengles.com/android-lesson-one-getting-started/
  */
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +33,46 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         //the bulk of the code the make sure we are using opengl2 is in the extended glsurfaceview.
         setContentView(new myGlSurfaceView(this));
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return  WindowInsetsCompat.CONSUMED;
+//        });
+        // 1. Hide the Action Bar if you are using a theme that includes one
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        // 2. Configure the window for immersive mode
+        Window window = getWindow();
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, window.getDecorView());
+
+        if (controller != null) {
+            // Hide both the status bar and the navigation bar
+            controller.hide(WindowInsetsCompat.Type.statusBars() | WindowInsetsCompat.Type.navigationBars());
+
+            // Set the behavior to "behavior system bars transient hopes"
+            // This allows the user to swipe from the edge to temporarily reveal the bars
+            controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+
     }
 
-    /*
-     * turns on immersive mode.  to get the navigation back, the user needs to swipe up
-     * from the bottom of the screen.
-     */
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        }
-    }
+//    /*
+//     * turns on immersive mode.  to get the navigation back, the user needs to swipe up
+//     * from the bottom of the screen.
+//     */
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus) {
+//            getWindow().getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+//        }
+//    }
 }
--- opengl3ex1/app/src/main/java/edu/cs4730/opengl3ex1/MainActivity.java
+++ opengl3ex1/app/src/main/java/edu/cs4730/opengl3ex1/MainActivity.java
@@ -1,29 +1,33 @@
 package edu.cs4730.opengl3ex1;
 
-import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.opengl.GLSurfaceView;
-import android.view.View;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 
 /**
  * This example use openGL v3.0
- *    this is based off of opengl2ex1 since 3.0 is backward compatible.
- *
- *  The render code is based off
- *   This code is based off of http://www.learnopengles.com/android-lesson-one-getting-started/
- *   but has been updated to use gl30 methods.
- *
- *   Note, there is no xml layout for this example.  It's all done in onCreate and the render.
+ * this is based off of opengl2ex1 since 3.0 is backward compatible.
+ * <p>
+ * The render code is based off
+ * This code is based off of http://www.learnopengles.com/android-lesson-one-getting-started/
+ * but has been updated to use gl30 methods.
+ * <p>
+ * Note, there is no xml layout for this example.  It's all done in onCreate and the render.
  *
  */
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     /**
      * Hold a reference to our GLSurfaceView
@@ -51,12 +55,30 @@ public class MainActivity extends Activity {
         }
         setContentView(mGLSurfaceView);
 
+        // 1. Hide the Action Bar if you are using a theme that includes one
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        //2. Configure the window for immersive mode
+        Window window = getWindow();
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(window, window.getDecorView());
+
+        if (controller != null) {
+            // Hide both the status bar and the navigation bar
+            controller.hide(WindowInsetsCompat.Type.statusBars() | WindowInsetsCompat.Type.navigationBars());
+
+            // Set the behavior to "behavior system bars transient hopes"
+            // This allows the user to swipe from the edge to temporarily reveal the bars
+            controller.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+
     }
 
 
     private boolean detectOpenGLES30() {
         ActivityManager am =
-                (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+            (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
         ConfigurationInfo info = am.getDeviceConfigurationInfo();
         return (info.reqGlEsVersion >= 0x30000);
     }
@@ -75,24 +97,4 @@ public class MainActivity extends Activity {
         super.onPause();
         mGLSurfaceView.onPause();
     }
-
-    /**
-     * turns on immersive mode.  to get the navigation back, the user needs to swipe up
-     * from the bottom of the screen.
-     */
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  //this line is api 19+
-
-        }
-    }
-
 }
